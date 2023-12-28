44c8d8a9403f8d352da43b8a0664ac3d61b4a484
Shubhangi <shubhangishubhra@gmail.com>
2023-12-28 20:22:59 +0530

    4

--- src/App.js
+++ src/App.js
@@ -1,43 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import Header from './components/Header';
-import Note from './components/Note';
-import SignIn from './signIn';
-import Home from './Home';
-import '../src/index.css';
+import React from 'react'
+import Header from './components/Header'
+import '../src/index.css'
+import Note from './components/Note'
 
 export const App = () => {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    // Check if a user is already signed in
-    const email = localStorage.getItem('email');
-    if (email) {
-      setUser({ email });
-    }
-  }, []);
-
-  const handleSignIn = (email) => {
-    setUser({ email });
-  };
-
-  const handleSignOut = () => {
-    localStorage.clear();
-    setUser(null);
-  };
-
   return (
     <div>
       <Header />
-      {user ? (
-        <div>
-          <Home user={user} onSignOut={handleSignOut} />
-          <Note />
-        </div>
-      ) : (
-        <SignIn onSignIn={handleSignIn} />
-      )}
-    </div>
-  );
-};
+     
+        
+        <Note />
+      </div>
 
-export default App;
+  
+  )
+}
+export default App
